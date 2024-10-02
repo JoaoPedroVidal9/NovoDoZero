c372fefe7d9ee2ca46cefd29e6dc85980065ed3f
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-10-02 10:06:10 -0300

    orgController adcionado e funcionando

--- PUT_DELETE/src/routes/apiRoutes.js
+++ PUT_DELETE/src/routes/apiRoutes.js
@@ -7,4 +7,11 @@ router.get("/user/", userController.getAllUsers);
 router.put("/user/", userController.updateUser);
 router.delete("/user/:cpf",userController.deleteUser);
 
+const orgController = require("../controller/orgController");
+
+router.post('/organizador', orgController.createOrg);
+router.get('/organizador', orgController.getAllOrg);
+router.put('/organizador', orgController.updateOrg);
+router.delete('/organizador/:id', orgController.deleteOrg);
+
 module.exports = router;
